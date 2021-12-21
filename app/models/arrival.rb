5704ac6f5637af099ec308719605ba6cd2e307b3
Arteen Wartanian <arteen.wartanian@9elements.com>
2021-12-21 13:10:52 +0100

    upcoming_arrivals method added

--- app/models/arrival.rb
+++ app/models/arrival.rb
@@ -2,4 +2,9 @@ class Arrival < ApplicationRecord
   belongs_to :transport
   belongs_to :station
   belongs_to :weekday
+
+  def self.upcoming_arrivals(station, weekday, time)
+    Arrival.includes(:station, :weekday).where(station: {id: station.id}, weekday: {id: weekday.id}).where("time > ?", time)
+  end
+
 end
